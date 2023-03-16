c5602c005ab1b236be433383a1ed922d91a1547e
ajit51 <68638451+ajit51@users.noreply.github.com>
2023-03-16 13:23:59 +0530

    Update DockerJenkinsIntegrationApplication.java

--- src/main/java/com/javatechie/spring/boot/docker/demo/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/javatechie/spring/boot/docker/demo/DockerJenkinsIntegrationApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerJenkinsIntegrationApplication {
+	
+	private Map<String, Object> map = new HashMap<>():
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
+	}
+	
+	public void loadDataToCache(){
+	
+		//logic
+	
 	}
 
 }
