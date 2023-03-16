18bab6c1b09a524f52933e503c457cec4ccdd1bf
ajit51 <68638451+ajit51@users.noreply.github.com>
2023-03-16 13:20:30 +0530

    Update DockerJenkinsIntegrationApplication.java

--- src/main/java/com/javatechie/spring/boot/docker/demo/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/javatechie/spring/boot/docker/demo/DockerJenkinsIntegrationApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerJenkinsIntegrationApplication {
+	
+	private static final String URL = "https://google.com";
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
